1f2faa711ae567acf2c523602de6ea5eab7477cd
ouzor <ouzor@iki.fi>
2014-10-09 16:44:59 +0300

    Updated RStudio Server and Shiny Server versions in attributes/default

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@
 default['rstudio']['cran']['mirror'] = 'http://cran.rstudio.com/'
 
 # APT configuration for Ubuntu or Debian installs.
-case node["platform"].downcase  
+case node["platform"].downcase
 when "ubuntu"
     default['rstudio']['apt']['key'] = 'E084DAB9'
     default['rstudio']['apt']['keyserver'] = 'keyserver.ubuntu.com'
@@ -13,7 +13,7 @@ when "debian"
     default['rstudio']['apt']['uri'] = 'http://cran.stat.ucla.edu/bin/linux/debian'
 end
 
-# You can define a simple array of packages in your role/environment/node and the 
+# You can define a simple array of packages in your role/environment/node and the
 # CRAN recipe will install them.
 default['rstudio']['cran']['packages'] = []
 
@@ -24,7 +24,7 @@ default['rstudio']['server']['www_address'] = '127.0.0.1'
 default['rstudio']['server']['ld_library_path'] = ''
 default['rstudio']['server']['r_binary_path'] = ''
 default['rstudio']['server']['user_group'] = ''
-default['rstudio']['server']['version'] = '0.98.1056'
+default['rstudio']['server']['version'] = '0.98.1062'
 default['rstudio']['server']['arch'] = node['kernel']['machine'] =~ /x86_64/ ? "amd64" : "i386"
 
 # RStudio Session
@@ -51,7 +51,7 @@ default['rstudio']['shiny']['log_dir'] = '/var/log/shiny-server'
 default['rstudio']['shiny']['directory_index'] = 'on'
 
 # Shiny can't be installe by APT. Don't get me started.
-default['rstudio']['shiny']['version'] = '0.4.0.8'
+default['rstudio']['shiny']['version'] = '1.2.2.367'
 default['rstudio']['shiny']['arch'] = node['kernel']['machine'] =~ /x86_64/ ? "amd64" : "i386"
 
 # Shiny server supports the users cookbook for HTTP Auth
